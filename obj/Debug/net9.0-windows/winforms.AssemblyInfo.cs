7c608fdc04e69b5a7d73c86cdc4cb2e599fc079c
unknown <gwayden@yandex.ru>
2025-02-27 01:12:49 +0500

    initial

--- obj/Debug/net9.0-windows/winforms.AssemblyInfo.cs
+++ obj/Debug/net9.0-windows/winforms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("winforms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+348fe645870b955d7c53c34f1eee809d0df679a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("winforms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("winforms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
